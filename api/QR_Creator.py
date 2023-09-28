0ca97e28468018b224eb87d3a9ac9134e60033ae
Aditya Gupta <77398939+Aditya152002@users.noreply.github.com>
2023-09-28 20:50:49 +0530

    Add files via upload

---
+++ api/QR_Creator.py
@@ -0,0 +1,22 @@
+#!C:\Users\Lenovo\AppData\Local\Programs\Python\Python311\python.exe
+from flask import Flask, url_for,render_template, jsonify,request
+app = Flask(__name__)
+qrimg = None
+check = False
+
+@app.route('/',methods =["GET","POST"])
+def generator():
+    check= False
+    return render_template('generator.html',qrimg= qrimg,check=check)
+@app.route('/Generator/Qrcode',methods =["GET","POST"])
+def check():
+    check =True
+    
+    qrdetail = None
+    if request.method == "POST":
+        qrdetail = request.form.get("qrdetail")
+    import qr_api as api
+    qrimg= api.Creator.qrcreator(qrdetail)
+    return render_template('generator.html',qrimg= qrimg,check=check)
+
+app.run(debug=True)
